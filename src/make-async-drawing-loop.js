697a4c268ecec7df6883d9d85905fcae3cf7f538
Emile Fokkema <emilefokkema@gmail.com>
2017-12-27 17:11:49 +0100

    cancel callbacks

--- src/make-async-drawing-loop.js
+++ src/make-async-drawing-loop.js
@@ -2,7 +2,7 @@ define(["viewbox"],function(viewBox){
 	var divideViewBox = function(b){
 		var size = Math.min(b.width, b.height) / 20;
 		var i = 0,
-			j = 0,
+			j = -1,
 			w = Math.floor(b.width / size) + 1,
 			h = Math.floor(b.height / size) + 1,
 			done = {done:true};
@@ -24,8 +24,38 @@ define(["viewbox"],function(viewBox){
 		return {next:next};
 	};
 
+	var callbackManagerFactory = function(){
+		var open = [];
+		var wrap = function(f){
+			var cancelled = false;
+			var result = function(){
+				if(!cancelled){
+					return f.apply(null, arguments);
+				}
+			};
+			result.cancel = function(){
+				cancelled = true;
+				var openIndex = open.indexOf(result);
+				if(openIndex > -1){
+					open.splice(openIndex, 1);
+				}
+			};
+			open.push(result);
+			return result;
+		};
+		return {
+			cancelOpen:function(){
+				var openCopy = open.slice();
+				openCopy.map(function(f){f.cancel();});
+			},
+			make:function(f){
+				return wrap(f);
+			}
+		};
+	};
+
 	return function(f, onDraw, currentContextTransform, c, cWrapper){ //f: function(ctx, viewBox, done)
-		var going, start, stop, dividedBox, drawNext, nextBox, currentId = 0;
+		var going, start, stop, dividedBox, drawNext, nextBox, currentId = 0, callbackManager = callbackManagerFactory();
 		start = function(){
 			console.log("start");
 			var latestViewBox = currentContextTransform.getTransformedViewBox();
@@ -40,9 +70,9 @@ define(["viewbox"],function(viewBox){
 					if(!nextBox.done){
 						currentContextTransform.removeTransform();
 						currentContextTransform.setTransform();
-						f(cWrapper, nextBox.value, function(){
+						f(cWrapper, nextBox.value, callbackManager.make(function(){
 							setTimeout(drawNext, 1);
-						});
+						}));
 						currentContextTransform.resetTransform();
 					}else{
 						stop();
@@ -54,7 +84,7 @@ define(["viewbox"],function(viewBox){
 		stop = function(){
 			console.log("stop");
 			going = false;
-			
+			callbackManager.cancelOpen();
 			
 		};
 		return {start:start,stop:stop};
